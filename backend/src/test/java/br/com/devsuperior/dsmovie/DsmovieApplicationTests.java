02c6029dfef5b76583d9fdf812ee2b41e30bba0a
Leandro Ikehara <leandro.ikehara@gmail.com>
2022-01-11 14:42:02 -0300

    Project created

---
+++ backend/src/test/java/br/com/devsuperior/dsmovie/DsmovieApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.devsuperior.dsmovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
