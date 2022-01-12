eefb418fa5444feb6a3053d1d6966b54ec415313
Leandro Ikehara <leandro.ikehara@gmail.com>
2022-01-12 00:55:56 -0300

    Routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,20 @@
-import Navbar from "./components/Navbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Listing from 'pages/Listing';
+import Form from 'pages/Form';
+import Navbar from "components/Navbar";
 
 function App() {
   return (
-
-    <Navbar></Navbar>
-
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Listing />} />
+        <Route path="/form">
+          <Route path=":movieId" element={<Form />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
-
 }
 
 export default App;
---
+++ frontend/src/pages/Listing/index.tsx
@@ -0,0 +1,8 @@
+function Listing() {
+
+    return(
+        <h1>Página de Listagem</h1>
+    );
+}
+
+export default Listing;
