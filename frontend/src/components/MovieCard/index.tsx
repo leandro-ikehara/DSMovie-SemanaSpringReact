260ca6e4039eef6ccc761dc59775eeea0b7991b5
Leandro Ikehara <leandro.ikehara@gmail.com>
2022-01-15 19:04:19 -0300

    Props

--- frontend/src/components/MovieCard/index.tsx
+++ frontend/src/components/MovieCard/index.tsx
@@ -1,16 +1,20 @@
 import MovieScore from "components/MovieScore";
 import { Link } from "react-router-dom";
+import { Movie } from "types/movies";
 
+type Props = {
+    movie: Movie;
+}
 
-function MovieCard() {
+function MovieCard({movie} : Props) {
 
-    const movie = {
-        id: 1,
-        image: "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg",
-        title: "The Witcher",
-        count: 2,
-        score: 4.5
-    };
+    // const movie = {
+    //     id: 1,
+    //     image: "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg",
+    //     title: "The Witcher",
+    //     count: 2,
+    //     score: 4.5
+    // };
 
     return (
         <div>
