7b15bbe681556eddfe17a2543c1cbb84e2edf540
Leandro Ikehara <leandro.ikehara@gmail.com>
2022-01-15 20:09:16 -0300

    Show score

--- frontend/src/components/MovieScore/index.tsx
+++ frontend/src/components/MovieScore/index.tsx
@@ -1,15 +1,20 @@
 import './moviescore-styles.css';
 import MovieStars from 'components/MovieStars';
 
-function MovieScore() {
+type Props = {
+    score : number;
+    count : number
+}
 
-    const score = 3.5;
-    const count = 13;
+function MovieScore({ score, count } : Props) {
+
+    // const score = 3.5;
+    // const count = 13;
 
     return (
         <div className="dsmovie-score-container">
             <p className="dsmovie-score-value">{score > 0 ? score.toFixed(1) : '-'}</p>
-            <MovieStars />
+            <MovieStars score={score}/>
             <p className="dsmovie-score-count">{count} avaliações</p>
         </div>
     );
